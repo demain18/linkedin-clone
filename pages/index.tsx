9afd823b0e0b6a8383120d4addf1c559cd7f6d52
백산 <hbfs0825@gmail.com>
2022-12-11 15:41:13 +0900

    add : development 환경에서 react-query 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from "next";
 import SnbProfile from "@/components/organisms/snbProfile/SnbProfile";
+import Gnb from "@/components/organisms/gnb/Gnb";
 
 const App: NextPage = () => {
   return (
     <>
+      <Gnb />
       <SnbProfile />
     </>
   );
