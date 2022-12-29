5f80aa5f09160dbf168332e682c2e3cc9fc9cea7
백산 <hbfs0825@gmail.com>
2022-12-29 18:48:40 +0900

    add : paletteDark 적용, 임시 비활성화

--- styles/paletteStyles.ts
+++ styles/paletteStyles.ts
@@ -13,15 +13,15 @@ export const palette = {
 };
 
 export const paletteDark = {
-  white: "#fff",
-  black: "#1b1c1d",
+  white: "#1b1c1d",
+  black: "#fff",
   primary: "#2185d0",
   gray: "#767676",
-  grayPoint1: "rgba(0, 0, 0, 0.1)",
-  grayPoint2: "rgba(0, 0, 0, 0.2)",
-  grayPoint4: "rgba(0, 0, 0, 0.4)",
-  grayPoint6: "rgba(0, 0, 0, 0.6)",
-  grayPoint8: "rgba(0, 0, 0, 0.8)",
+  grayPoint1: "rgba(255, 255, 255, 0.1)",
+  grayPoint2: "rgba(255, 255, 255, 0.2)",
+  grayPoint4: "rgba(255, 255, 255,0.4)",
+  grayPoint6: "rgba(255, 255, 255, 0.6)",
+  grayPoint8: "rgba(255, 255, 255, 0.8)",
   lightGray: "#ececec",
-  background: "#f3f2ef",
+  background: "#323232",
 };
