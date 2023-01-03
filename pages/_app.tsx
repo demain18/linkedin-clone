c9eb0b4fcedefb53cadded4608dc568b0dcf9690
백산 <hbfs0825@gmail.com>
2023-01-03 21:25:01 +0900

    add : theme 적용 준비

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { Provider } from "react-redux";
 import store from "../modules/store";
+import styled, { ThemeProvider } from "styled-components";
 
 // msw 적용
 if (typeof global.process === "undefined") {
@@ -12,14 +13,30 @@ if (typeof global.process === "undefined") {
   worker.start();
 }
 
+const paletteLight = {
+  white: "#fff",
+  black: "#1b1c1d",
+  primary: "#2185d0",
+  gray: "#767676",
+  grayPoint1: "rgba(0, 0, 0, 0.1)",
+  grayPoint2: "rgba(0, 0, 0, 0.2)",
+  grayPoint4: "rgba(0, 0, 0, 0.4)",
+  grayPoint6: "rgba(0, 0, 0, 0.6)",
+  grayPoint8: "rgba(0, 0, 0, 0.8)",
+  lightGray: "#ececec",
+  background: "#f3f2ef",
+};
+
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-        <ReactQueryDevtools />
+        <ThemeProvider theme={paletteLight}>
+          <Component {...pageProps} />
+          <ReactQueryDevtools />
+        </ThemeProvider>
       </QueryClientProvider>
     </Provider>
   );
--- styles/paletteStyles.ts
+++ styles/paletteStyles.ts
@@ -1,3 +1,48 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../modules/store";
+
+// let paletteNow: any;
+
+// export const palette = () => {
+//   paletteNow = useSelector((state: RootState) => state.global.themeIsLight);
+//   console.log(paletteNow);
+
+//   let palette;
+
+//   if (paletteNow === true) {
+//     palette = {
+//       white: "#fff",
+//       black: "#1b1c1d",
+//       primary: "#2185d0",
+//       gray: "#767676",
+//       grayPoint1: "rgba(0, 0, 0, 0.1)",
+//       grayPoint2: "rgba(0, 0, 0, 0.2)",
+//       grayPoint4: "rgba(0, 0, 0, 0.4)",
+//       grayPoint6: "rgba(0, 0, 0, 0.6)",
+//       grayPoint8: "rgba(0, 0, 0, 0.8)",
+//       lightGray: "#ececec",
+//       background: "#f3f2ef",
+//     };
+//   } else {
+//     palette = {
+//       white: "#1b1c1d",
+//       black: "#fff",
+//       primary: "#2185d0",
+//       gray: "#767676",
+//       grayPoint1: "rgba(255, 255, 255, 0.1)",
+//       grayPoint2: "rgba(255, 255, 255, 0.2)",
+//       grayPoint4: "rgba(255, 255, 255,0.4)",
+//       grayPoint6: "rgba(255, 255, 255, 0.6)",
+//       grayPoint8: "rgba(255, 255, 255, 0.8)",
+//       lightGray: "#ececec",
+//       background: "#323232",
+//     };
+//   }
+
+//   return palette;
+// };
+
 export const palette = {
   white: "#fff",
   black: "#1b1c1d",
