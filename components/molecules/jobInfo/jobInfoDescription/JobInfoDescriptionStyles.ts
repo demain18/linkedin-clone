bac396ae8e929af2e7b93874cccdecd7720dde98
백산 <hbfs0825@gmail.com>
2023-01-17 16:41:02 +0900

    add : JobInfoDescription 구현

---
+++ components/molecules/jobInfo/jobInfoDescription/JobInfoDescriptionStyles.ts
@@ -0,0 +1,7 @@
+import styled, { css } from "styled-components";
+import { Props } from "./JobInfoDescription";
+
+export const JobInfoDescriptionStyled = styled.div<Props>`
+  white-space: pre-wrap;
+  line-height: 27px;
+`;
