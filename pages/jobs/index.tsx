71b6108905caf00581d66655ca053366e56667fb
백산 <hbfs0825@gmail.com>
2023-01-19 17:03:40 +0900

    add : jobs 페이지 레이아웃 구현

--- pages/jobs/index.tsx
+++ pages/jobs/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from "next";
 import Gnb from "@/components/organisms/gnb/Gnb";
-import {} from "./indexStyles";
+import { GlobalWrap, JobInfoWrap, JobListWrap } from "./indexStyles";
 import { Frame, FrameWrap } from "@/styles/moduleStyles";
+import JobListHeader from "@/components/molecules/jobList/jobListHeader/JobListHeader";
+import JobListContent from "@/components/molecules/jobList/jobListContent/JobListContent";
+import JobInfoHeader from "@/components/molecules/jobInfo/jobInfoHeader/JobInfoHeader";
+import JobInfoTags from "@/components/molecules/jobInfo/jobInfoTags/JobInfoTags";
+import JobInfoDescription from "@/components/molecules/jobInfo/jobInfoDescription/JobInfoDescription";
+import JobInfoAbout from "@/components/molecules/jobInfo/jobInfoAbout/JobInfoAbout";
 
 export interface Props {}
 
@@ -10,7 +16,20 @@ const App: NextPage = () => {
     <>
       <Gnb />
       <Frame>
-        <FrameWrap>Jobs</FrameWrap>
+        <FrameWrap>
+          <GlobalWrap>
+            <JobListWrap>
+              <JobListHeader />
+              <JobListContent />
+            </JobListWrap>
+            <JobInfoWrap>
+              <JobInfoHeader />
+              <JobInfoTags />
+              <JobInfoDescription desc="this is description" />
+              <JobInfoAbout />
+            </JobInfoWrap>
+          </GlobalWrap>
+        </FrameWrap>
       </Frame>
     </>
   );
--- pages/jobs/indexStyles.ts
+++ pages/jobs/indexStyles.ts
@@ -1,4 +1,21 @@
 import styled, { css } from "styled-components";
 import { Props } from "./index";
 
-export const indexStyled = styled.div<Props>``;
+export const GlobalWrap = styled.div`
+  width: 100%;
+  border: 1px solid blue;
+  display: flex;
+`;
+
+export const JobListWrap = styled.div`
+  width: 450px;
+`;
+
+export const JobInfoWrap = styled.div`
+  width: 100%;
+  padding: 20px;
+  background-color: ${(props) => props.theme.white};
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
