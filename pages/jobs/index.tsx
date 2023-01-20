1f599bffcf5807f1d08e0a0c273cfd98ee18464e
백산 <hbfs0825@gmail.com>
2023-01-20 16:04:59 +0900

    fix : 안쓰는 코드 제거

--- pages/jobs/index.tsx
+++ pages/jobs/index.tsx
@@ -2,12 +2,6 @@ import type { NextPage } from "next";
 import Gnb from "@/components/organisms/gnb/Gnb";
 import { GlobalWrap, JobInfoWrap, JobListWrap } from "./indexStyles";
 import { Frame, FrameWrap } from "@/styles/moduleStyles";
-import JobListHeader from "@/components/molecules/jobList/jobListHeader/JobListHeader";
-import JobListContent from "@/components/molecules/jobList/jobListContent/JobListContent";
-import JobInfoHeader from "@/components/molecules/jobInfo/jobInfoHeader/JobInfoHeader";
-import JobInfoTags from "@/components/molecules/jobInfo/jobInfoTags/JobInfoTags";
-import JobInfoDescription from "@/components/molecules/jobInfo/jobInfoDescription/JobInfoDescription";
-import JobInfoAbout from "@/components/molecules/jobInfo/jobInfoAbout/JobInfoAbout";
 import JobList from "@/components/organisms/jobList/JobList";
 import JobInfo from "@/components/organisms/jobInfo/JobInfo";
 
